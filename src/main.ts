382c389e0f92a2fb49838110f0f77290721f3bc7
rajakumaran1203 <44520800+rajakumaran1203@users.noreply.github.com>
2023-12-28 23:56:17 +0530

    origin

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     credentials: true,
   });
   const port = 3000;
